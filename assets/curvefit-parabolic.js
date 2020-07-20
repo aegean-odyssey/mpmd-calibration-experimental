20b4a47fffae15d1b0592ac84d404a6b86c9e292
Aegean Odyssey <57586704+aegean-odyssey@users.noreply.github.com>
2020-07-20 19:36:18 +0000

    tweaks, add linear curve-fit

--- assets/curvefit-parabolic.js
+++ assets/curvefit-parabolic.js
@@ -22,16 +22,22 @@ function curvefit(s, f) {
     const FILENAME = 'G29_BEDFIX.gcode';
     const MIMETYPE = 'text/x-gcode';
     
+    FW = s.match(/FIRMWARE_NAME:[^\)]+\)/);
+    M92  = s.match(/M92.+/);
+    M665 = s.match(/M665.+/);
+    M666 = s.match(/M666.+/);
+    M851 = s.match(/M851.+/);
+
     s = s.split("\n");
     s = s.filter(function(u) { return u.includes('Bed X') });
-    s = s.map(function(u) { return u.match(/[-+.0-9]+/g) });
+    var SS = s.map(function(u) { return u.match(/[-+.0-9]+/g) });
 
     function datum(x, y) {
         return [ x*x, y*y, x*y, x, y, 1.0 ];
     }
 
-    var XY = s.map(function(u) { return datum(+u[0], +u[1]) });
-    var Z  = s.map(function(u) { return +u[2] });
+    var XY = SS.map(function(u) { return datum(+u[0], +u[1]) });
+    var Z  = SS.map(function(u) { return +u[2] });
 
     var b = math.transpose(math.matrix(Z));
     var A = math.matrix(XY);
@@ -52,7 +58,7 @@ function curvefit(s, f) {
         return math.round(((2 * n - (N - 1)) * R) / (N - 1));
     }
 
-    var s = [];
+    s = [];
 
     function o(u, v) {
         s.push(((v === undefined) ? u : math.print(u, v)) + "\n");
@@ -70,16 +76,24 @@ function curvefit(s, f) {
         return s + math.format(v, w) + f;
     }
 
-    o('G28');
+    if (FW  ) o('; $0', FW);
+    if (M92 ) o('; $0', M92);
+    if (M665) o('; $0', M665);
+    if (M666) o('; $0', M666);
+    if (M851) o('; $0', M851);
+    SS.forEach(function(xyz) { o('; Bed X: $0 Y: $1 Z: $2', xyz) });
+
+    o('; ');
     o('; bilinear bed level mesh, least-square fit to a paraboloid');
     o('; z = $0 x^2 + $1 y^2 + $2 x y + $3 x + $4 y + $5', a(FIT));
     o('; residual = $0', [ n(RES,5) ]);
+    o('G28 ; MUST home before using G29 code');
     o('G29 L$0 R$1 B$0 F$1 C0 ; $2 x $2', [ -R, R, N ]);
     for (var j = 0; j < N; j++) {
         var y = grid(j);
         for (var i = 0; i < N; i++) {
             var x = grid(i);
-            o('G29 W I$0 J$1 Z$2  ; X$3 Y$4',
+            o('G29 W I$0 J$1 Z$2; X$3 Y$4',
 	      [ i, j, n(z(x, y),5), n(x,1), n(y,1) ]);
         }
     }
--- index.md
+++ index.md
@@ -6,5 +6,6 @@ title: MPMD Calibration
 Here are some scripts that can be used to post-process the bed-level mesh of Monoprice MP Mini Delta 3d printer with [mpmd_marlin_1.1.x firmware](https://github.com/aegean-odyssey/mpmd_marlin_1.1.x) installed. This work is purely experimental -- the objective is to develop ways to evaluate and improve the calibration procedures in the mpmd_marlin_1.1.x firmware.
 
 ### Online Tools
+* [linear curve-fit](curvefit-linear.html)
 * [parabolic curve-fit](curvefit-parabolic.html)
 * _more to come..._
