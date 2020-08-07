8eaea206abd3e46dd351334d98a605f83dee3200
Aegean Odyssey <57586704+aegean-odyssey@users.noreply.github.com>
2020-08-07 19:03:23 +0000

    fixes to pattern pages

--- assets/patterns.js
+++ assets/patterns.js
@@ -27,56 +27,68 @@ function X(x, y) {
 }
 
 function g30(pattern, name) {
+
     const FILENAME = name;
     const MIMETYPE = "text/x-gcode";
 
-    var s = [];
-    s.push('M988 /G30PROBE.TXT');
-    s.push('M115');
-    s.push('M503');
-    s.push('G28');
+    var ss = [];
+
+    function s(u) {
+	ss.push(u + '\n');
+    }
+    
+    s('M988 /G30PROBE.TXT');
+    s('M115');
+    s('M503');
+    s('G28');
     for (const i in pattern) {
         const a = pattern[i];
-        s.push(math.print('G30 X$0 Y$1 V1', a));
+        s(math.print('G30 X$0 Y$1 V1', a));
     }
-    s.push('G1 X0 Y0 Z40');
-    s.push('M400#');
-    s.push('M989');
-    s.push('M118 {TQ\\:100}{SYS\\:STARTED}#');
-    s.push('M118 {E\:Done! (see /G30PROBE.TXT)}#\n');
-    return [ 0, s.join('\n'), FILENAME, MIMETYPE ];
+    s('G1 X0 Y0 Z40');
+    s('M400#');
+    s('M989');
+    s('M118 {TQ\\:100}{SYS\\:STARTED}#');
+    s('M118 {E\:Done! (see /G30PROBE.TXT)}#');
+    return [ 0, ss, FILENAME, MIMETYPE ];
 }
 
 function bed(pattern, name) {
 
-    var s = [];
-    s.push('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" ');
-    s.push(' width="100%" height="100%" viewBox="-600 -600 1200 1200"');
-    s.push(' preserveAspectRatio="xMidYMid meet">');
-    s.push(' <g transform="scale(1,-1)" stroke-width="2" stroke="steelblue">');
-    s.push('  <defs>');
-    s.push('   <marker id="arrow" orient="auto" markerUnits="strokeWidth"');
-    s.push('    markerWidth="11" markerHeight="7" refX="18" refY="3">');
-    s.push('    <path d="M0,0 L10,3 L0,6 L4,3 z" fill="steelblue"/>');
-    s.push('   </marker>');
-    s.push('  </defs>');
-    s.push('  <circle cx="0" cy="0" r="550" stroke="grey" fill="none"/>');
-    const point = '  <circle cx="$0" cy="$1" r="10" stroke="black" fill="black"/>';
+    var ss = [];
+
+    function s(u) {
+	ss.push(u + '\n');
+    }
+    
+    s('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" ');
+    s(' width="100%" height="100%" viewBox="-600 -600 1200 1200"');
+    s(' preserveAspectRatio="xMidYMid meet">');
+    s(' <g transform="scale(1,-1)" stroke-width="2" stroke="steelblue">');
+    s('  <defs>');
+    s('   <marker id="arrow" orient="auto" markerUnits="strokeWidth"');
+    s('    markerWidth="11" markerHeight="7" refX="18" refY="3">');
+    s('    <path d="M0,0 L10,3 L0,6 L4,3 z" fill="steelblue"/>');
+    s('   </marker>');
+    s('  </defs>');
+    s('  <circle cx="0" cy="0" r="550" stroke="grey" fill="none"/>');
+    const point = '  <circle cx="$0" cy="$1" r="12" stroke="white" fill="black"/>';
     const arrow = '  <line x1="$0" y1="$1" x2="$2" y2="$3" marker-end="url(#arrow)"/>';
+    pattern.reverse();  // display in reverse for cleaner overlapping arrows
     var a = pattern.shift();
-    s.push(math.print(point, a));
+    s(math.print(point, a));
     for (const i in pattern) {
         const b = pattern[i];
-        s.push(math.print(arrow, a.concat(b)));
-        s.push(math.print(point, b));
+        s(math.print(arrow, b.concat(a)));
+        s(math.print(point, b));
         a = b;
     }
     const tower = '  <g transform="rotate($0)">$1</g>';
     const T = '<line x1="0" y1="535" x2="0" y2="570" stroke-width="10"/>';
-    s.push(math.print(tower, [0, T]));
-    s.push(math.print(tower, [120, T]));
-    s.push(math.print(tower, [240, T]));
-    s.push(' </g>');
-    s.push('</svg>\n');
-    return s.join('\n');
+    s(math.print(tower, [0, T]));
+    s(math.print(tower, [120, T]));
+    s(math.print(tower, [240, T]));
+    s(' </g>');
+    s('</svg>');
+    return s.join('');
 }
